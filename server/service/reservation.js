da2763c65eef8cc9860adb006007c4852c314cef
Varun Vasudevan <the.flazer@gmail.com>
2017-04-12 11:45:04 +0800

    Added tests for Reservation function
    Max Renting Period can now be equal to Min Renting Period

--- server/service/reservation.js
+++ server/service/reservation.js
@@ -1,32 +1,47 @@
 const _ = require('lodash');
 const moment = require('moment');
 
-const isAvailable = (allReservationsProps, newReservationProps) => {
+const isAvailable = (allReservationsProps, newReservationProps, item = {}) => {
   const newReservation = _.pick(newReservationProps, ['startTime', 'endTime']);
   const allReservations = _.map(allReservationsProps, (reservation) => {
     if (!moment(_.get(reservation, 'startTime', null)).isValid()) {
-      console.log('throwing error');
-      throw new Error(`Invalid start time for old reservation ${reservation}`);
+      throw new Error(`Invalid start time for old reservation ${JSON.stringify(reservation)}`);
     }
     if (!moment(_.get(reservation, 'endTime', null)).isValid()) {
-      throw new Error(`Invalid end time for old reservation ${reservation}`);
+      throw new Error(`Invalid end time for old reservation ${JSON.stringify(reservation)}`);
     }
     return _.pick(reservation, ['startTime', 'endTime']);
   });
-  if (!moment(_.get(newReservation, 'startTime', null)).isValid()) {
-    throw new Error(`Invalid start time for new reservation ${newReservation}`);
+  const newStartTime = _.get(newReservation, 'startTime', null);
+  const newEndTime = _.get(newReservation, 'endTime', null);
+  if (!moment(newStartTime).isValid()) {
+    throw new Error(`Invalid start time for new reservation ${JSON.stringify(newReservation)}`);
   }
-  if (!moment(_.get(newReservation, 'endTime', null)).isValid()) {
-    throw new Error(`Invalid end time for new reservation ${newReservation}`);
+  if (!moment(newEndTime).isValid()) {
+    throw new Error(`Invalid end time for new reservation ${JSON.stringify(newReservation)}`);
+  }
+  if (moment(newEndTime).isSameOrBefore(newStartTime, 'day')) {
+    throw new Error(`End date (${newEndTime}) is either on or before the Start date ${newStartTime}`);
   }
   _.forEach(allReservations, (reservation) => {
-    console.log(newReservation.startTime);
-    console.log(reservation.startTime, reservation.endTime);
     if (moment(newReservation.startTime).isBetween(reservation.startTime, reservation.endTime, null, '[]')) {
       throw new Error('Your reservation conflicts with another ' +
         `reservation made between ${reservation.startTime} and ${reservation.endTime}`);
     }
   });
+  const diff = moment(newEndTime).diff(moment(newStartTime), 'days');
+  if (_.get(item, 'minRentingPeriod', null) != null) {
+    if (diff < parseInt(item.minRentingPeriod, 10)) {
+      throw new Error(`The duration of your reservation (${diff} days) is shorter than the minimum ` +
+      `renting period (${item.minRentingPeriod} days) required to rent this item`);
+    }
+  }
+  if (_.get(item, 'maxRentingPeriod', null) != null) {
+    if (diff > parseInt(item.maxRentingPeriod, 10)) {
+      throw new Error(`The duration of your reservation (${diff} days) is longer than the maximum ` +
+      `renting period (${item.minRentingPeriod} days) that is allowed to rent this item`);
+    }
+  }
   return Promise.resolve(newReservationProps);
 };
 
---
+++ server/service/reservation.spec.js
@@ -0,0 +1,128 @@
+'use strict';
+
+let chai = require('chai');
+let expect = chai.expect;
+let reservationService = require('./reservation');
+
+const validPastReservations = [{
+  startTime: '2017-04-11T10:40:20.815Z',
+  endTime: '2017-04-15T10:40:20.815Z',
+}, {
+  startTime: '2017-04-16T10:40:20.815Z',
+  endTime: '2017-04-20T10:40:20.815Z',
+}];
+
+const sampleItemProps = {
+  id: 1,
+  title: 'Nikon D500',
+  brand: 'Nikon',
+  model: 'd500',
+  condition: 'new',
+  description: 'Something',
+  purchaseDate: '2017-04-10T07:31:51.597Z',
+  photos: [
+    'http://kenrockwell.com/nikon/images1/d500/1600.jpg',
+  ],
+  minRentingPeriod: 10,
+  maxRentingPeriod: 15,
+  availability: [[
+    '2017-04-10T07:31:51.597Z',
+    '2017-04-30T07:31:51.597Z',
+  ]],
+  price: 50,
+  deposit: 100,
+  length: 100,
+  height: 100,
+  width: 100,
+  ownerId: 2,
+};
+
+describe('Reservation Service', () => {
+  it('Given a valid set of prior reservations and a valid new reservation,' +
+  ' the reservation service will validate it successfully', (done) => {
+    const newReservation = {
+      startTime: '2017-04-21T10:40:20.815Z',
+      endTime: '2017-04-25T10:40:20.815Z',
+    };
+    reservationService.isAvailable(validPastReservations, newReservation)
+    .then((validReservation) => {
+      expect(validReservation.startTime).to.equal(newReservation.startTime);
+      expect(validReservation.endTime).to.equal(newReservation.endTime);
+      done();
+    });
+  });
+
+  it('Given a valid set of prior reservations and a new reservation' +
+  ' where end date < start date, it fails to be validated', (done) => {
+    const newReservation = {
+      startTime: '2017-04-10T10:40:20.815Z',
+      endTime: '2017-04-01T10:40:20.815Z',
+    };
+    const composedFunction = () => {
+      Promise.resolve(reservationService.isAvailable(validPastReservations, newReservation));
+    };
+    expect(composedFunction).to.throw(/End date/);
+    done();
+  });
+
+  it('Given an invalid set of prior reservations (malformed object) and a valid new reservation' +
+  ' it fails to be validated', (done) => {
+    const invalidPastReservations = [{
+      what: 'are you reading this for?',
+      cos: 'this is supposed to be invalid',
+    }, {
+      really: 'you\'re wasting even more time just like',
+      endTime: '2017-04-01T10:40:20.815Z',
+    }];
+    const newReservation = {
+      startTime: '2017-04-10T10:40:20.815Z',
+      endTime: '2017-04-01T10:40:20.815Z',
+    };
+    const composedFunction = () => {
+      Promise.resolve(reservationService.isAvailable(invalidPastReservations, newReservation));
+    };
+    expect(composedFunction).to.throw(/Invalid/);
+    done();
+  });
+
+  it('Given a valid set of prior reservations and an invalid new reservation' +
+  ' (malformed object) it fails to be validated', (done) => {
+    const newReservation = {
+      what: 'are you reading this for?',
+      cos: 'this is supposed to be invalid',
+    };
+    const composedFunction = () => {
+      Promise.resolve(reservationService.isAvailable(validPastReservations, newReservation));
+    };
+    expect(composedFunction).to.throw(/Invalid/);
+    done();
+  });
+
+  it ('Given a valid set of prior reservations, invalid new reservation ' +
+  '(duration shorter than item\'s minimum renting period) and an item\'s details, ' +
+  ' it fails to be validated', (done) => {
+    const newReservation = {
+      startTime: '2017-04-10T10:40:20.815Z',
+      endTime: '2017-04-15T10:40:20.815Z',
+    };
+    const composedFunction = () => {
+      Promise.resolve(reservationService.isAvailable(validPastReservations, newReservation, sampleItemProps));
+    };
+    expect(composedFunction).to.throw(/shorter than the minimum renting period/);
+    done();
+  });
+
+  it ('Given a valid set of prior reservations, invalid new reservation ' +
+  '(duration longer than item\'s minimum renting period) and an item\'s details, ' +
+  ' it fails to be validated', (done) => {
+    const newReservation = {
+      startTime: '2017-04-01T10:40:20.815Z',
+      endTime: '2017-04-30T10:40:20.815Z',
+    };
+    const composedFunction = () => {
+      Promise.resolve(reservationService.isAvailable(validPastReservations, newReservation, sampleItemProps));
+    };
+    expect(composedFunction).to.throw(/longer than the maximum renting period/);
+    done();
+  });
+});
