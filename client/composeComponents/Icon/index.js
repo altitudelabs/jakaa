8166c25691736bb968348a9ea8b39b86eb4bbd5a
Declan Elcocks <declan@altitudelabs.com>
2017-05-04 14:09:38 +0800

    feature(icon): Tidy Icon syntax

--- client/composeComponents/Icon/index.js
+++ client/composeComponents/Icon/index.js
@@ -4,7 +4,12 @@ import React from 'react';
 const Icon = ({ icon }) => {
   const svg = require(`raw-loader!./icons/${icon}.svg`);
 
-  return <div className={'icon'} dangerouslySetInnerHTML={{ __html: svg }} />;
+  return (
+    <div
+      className={'icon'}
+      dangerouslySetInnerHTML={{ __html: svg }}
+    />
+  );
 }
 
 export default Icon;
