c434561c24587e1a615654e3cb1e1009db41270a
Declan Elcocks <declan@altitudelabs.com>
2017-05-04 15:03:10 +0800

    feature(footer): Add Links to footer

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -1,11 +1,37 @@
 import './style.scss';
 import React from 'react';
+import { Link } from 'react-router';
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        Footer
+        <ul>
+          <li>
+            <Link to={'/fa'}>FA</Link>
+          </li>
+
+          <li>
+            <Link to={'/contact'}>Contact</Link>
+          </li>
+
+          <li>
+            <Link to={'/terms'}>Terms</Link>
+          </li>
+
+          <li>
+            <Link to={'/privacy'}>Privacy</Link>
+          </li>
+
+        </ul>
+      </div>
+
+      <div>
+        section 2
+      </div>
+
+      <div>
+        section 3
       </div>
     </footer>
   );
