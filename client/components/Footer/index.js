40d8d00ae62b3eb4776dd458c6c46c372b0d5be5
Declan Elcocks <declan@altitudelabs.com>
2017-05-04 14:43:27 +0800

    feature(footer): Add Footer component scaffolding

---
+++ client/components/Footer/index.js
@@ -0,0 +1,14 @@
+import './style.scss';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        Footer
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
